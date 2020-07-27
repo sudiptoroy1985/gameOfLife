b12b7460120f0863e04bcb873206637e320b1fed
sudipto roy <sudipto.roy.1985@gmail.com>
2020-07-27 13:55:53 +0530

    refactor board get cell

--- src/Board.js
+++ src/Board.js
@@ -10,9 +10,8 @@ export class Board {
     }
 
     getCell(current, endIdx){
-        let currentIdx = this.cells.indexOf(current);
-        let toTraverseIndex = currentIdx + endIdx;
-        if((currentIdx + endIdx) >= this.cells.length){
+        let toTraverseIndex = this.cells.indexOf(current) + endIdx;
+        if(toTraverseIndex >= this.cells.length){
           return this.cells[toTraverseIndex - this.cells.length]
         }
         return this.cells[toTraverseIndex];
